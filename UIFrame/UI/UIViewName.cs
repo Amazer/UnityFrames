c042e2ccb25d22f6f6d864727a982e6e9e99fab0
Amazer <149779511@qq.com>
2021-03-13 15:30:43 +0800

    修复框架缺少的文件

---
+++ UIFrame/UI/UIViewName.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// UIPanel的名称，和对应的Prefab名字一致
+/// </summary>
+public enum UIViewName  {
+    StartPanel,
+    QuestionPanel,
+    PapeReviewPanel,
+    SettingPanel,
+}
