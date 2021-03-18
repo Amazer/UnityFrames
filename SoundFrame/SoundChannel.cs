5f07be4f77c9b464845e65767214725dc24e84f4
Amazer <149779511@qq.com>
2021-03-18 20:15:06 +0800

    添加声音模块

---
+++ SoundFrame/SoundChannel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundChannel : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    public AudioSource audioSource => _audioSource;
+
+    void Awake()
+    {
+        _audioSource = gameObject.GetComponent<AudioSource>();
+        if (_audioSource == null)
+        {
+            _audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public void PlayOneShot(AudioClip clip)
+    {
+        _audioSource.PlayOneShot(clip);
+    }
+    public void Play(AudioClip clip)
+    {
+        _audioSource.clip = clip;
+        _audioSource.Play();
+    }
+
+    public void Stop()
+    {
+        _audioSource.Stop();
+    }
+
+    public void Mute(bool mute)
+    {
+        _audioSource.mute = mute;
+    }
+
+    public static SoundChannel CreateChannel(Transform parent,string name="Channel")
+    {
+        GameObject go = new GameObject(name);
+        SoundChannel channel = go.AddComponent<SoundChannel>();
+        go.transform.SetParent(parent);
+        go.transform.localPosition = Vector3.zero;
+        return channel;
+    }
+
+    public static void DestroyChannel(SoundChannel channel)
+    { 
+        GameObject.Destroy(channel.gameObject);
+    }
+}
---
+++ SoundFrame/SoundMgr.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMgr : MonoBehaviour
+{
+    public List<SoundChannel> channelsList = new List<SoundChannel>();
+    private SoundChannel bgmChannel;
+    private SoundChannel clickChannel;
+
+    public SoundChannel BGMChannel
+    {
+        get
+        {
+            if (bgmChannel)
+            {
+                bgmChannel = SoundChannel.CreateChannel(this.transform, "BGM");
+                channelsList.Add(bgmChannel);
+            }
+            return bgmChannel;
+        }
+    }
+    public SoundChannel ClickChannel
+    {
+        get
+        {
+            if (clickChannel)
+            {
+                clickChannel = SoundChannel.CreateChannel(this.transform, "click");
+                channelsList.Add(clickChannel);
+            }
+            return bgmChannel;
+        }
+    }
+
+    public void MuteAll(bool mute)
+    {
+        var iter = channelsList.GetEnumerator();
+        while (iter.MoveNext())
+        {
+             iter.Current.Mute(mute);
+        }
+    }
+    public void StopAll()
+    {
+        var iter = channelsList.GetEnumerator();
+        while (iter.MoveNext())
+        {
+             iter.Current.Stop();
+        }
+    }
+}
